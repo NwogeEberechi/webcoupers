46ec82bbb0cf348ebe5b7922ae4dcd3e1a5d339a
NwogeEberechi <eberechinwoge@gmail.com>
2018-12-20 16:56:28 +0100

    initial commit

---
+++ assets/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+
+      $(".dropdown-trigger").dropdown();
+
+      $(".dropdown-trigger2").dropdown();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
